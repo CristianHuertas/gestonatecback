b1790d8920614e7a1caf2841d9f9ad455b15260a
CristianHuertas <cristian.huertas0@gmail.com>
2020-12-23 12:01:47 -0500

    se incluyen credenciales para coneccion a heroku

--- db/db_conexion.py
+++ db/db_conexion.py
@@ -1,14 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
 
 #Creando Motor y Conexion con la Base de Datos
 # TODO: Con la ayuda de pgAdmin debes crear una base de datos
 # y la BD debes crearle un esquema
 # TODO: Debes cambiar con tu usuario y contraseña de PostgreSQL
 # además del host, puerto y nombre de la base de datos que hayas definido
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/MISION_TIC"
-engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
+import os
+
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/MISION_TIC"
+engine = create_engine(DATABASE_URL)
 
 #Creacion de un creador la Sesion
 SessionLocal = sessionmaker(autocommit=False,
